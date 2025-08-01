d28836e1f36e0a9e9090ce3c9c7f3caa98d5cc07
YishanWang123 <yishanwang_cs@163.com>
2025-08-01 22:09:46 +0800

    can run succ

---
+++ t2i.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+export HF_ENDPOINT=https://hf-mirror.com
+export MODEL_NAME="stable-diffusion-v1-5/stable-diffusion-v1-5"
+export TRAIN_DIR="/root/diffusers/memsdata/train"
+export OUTPUT_DIR="/root/diffusers/output"
+
+accelerate launch examples/text_to_image/train_text_to_image_lora.py \
+  --pretrained_model_name_or_path=$MODEL_NAME \
+  --train_data_dir=$TRAIN_DIR \
+  --output_dir=$OUTPUT_DIR \
+  --resolution=512 \
+  --train_batch_size=2 \
+  --gradient_accumulation_steps=4 \
+  --learning_rate=1e-4 \
+  --max_train_steps=3 \
+  --validation_prompt="TBD" \
+  --report_to="wandb"
